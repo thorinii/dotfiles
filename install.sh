48babca56a5cfd1173244eb732c57bb60edd41e5
Lachlan Phillips <flipsidelachy@gmail.com>
2020-02-01 17:37:23 +1030

    Refactored install script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-######################
-# Symlinks the dotfiles into the home directory
-######################
+
+###############################
+# Installs everything useful. #
+###############################
+
 
 basedir="$(cd "$(dirname "$0")" || exit 1; pwd)"
 targetdir="$HOME"
@@ -13,18 +15,46 @@ liquidprompt_dir="$vendor_dir/liquidprompt"
 vimplug_dir="$vendor_dir/vim-plug"
 
 
-link_dotfile () {
-  local file="$1"
-  local source="$basedir/$file"
-  local target="$targetdir/.$file"
+clone_repo () {
+  local repo="${1}"
+  local target="${2}"
 
-  if [ -e "$target" ]; then
-    echo "- backing up $file"
+  local name="$(basename "$target")"
+
+  echo "$name:"
+  if [[ -e "$target" ]]; then
+    echo "- removing existing repo"
+    rm -rf "$target"
+  fi
+
+  echo "- cloning $repo"
+  git clone "$repo" "$target"
+}
+
+
+link_dotfile () {
+  local source="${1}"
+  local target="${2}"
+
+  local file="$(basename "$source")"
+  local target_parent="$(dirname "$target")"
+
+  echo "$file:"
+  if [[ -h "$target" ]]; then
+    echo "- removing existing symlink"
+    rm "$target"
+  elif [[ -e "$target" ]]; then
+    echo "- backing up existing"
     mkdir -p "$backup_dir"
     mv "$target" "$backup_dir/$file"
   fi
 
-  echo "- linking $file"
+  if [[ ! -e "$target_parent" ]]; then
+    echo "- creating parent dir"
+    mkdir -p "$target_parent"
+  fi
+
+  echo "- linking"
   ln -s "$source" "$target"
 }
 
@@ -32,17 +62,12 @@ link_dotfile () {
 
 mkdir -p "$vendor_dir"
 
+clone_repo "https://github.com/nojhan/liquidprompt.git" "$liquidprompt_dir"
+clone_repo "https://github.com/junegunn/vim-plug.git" "$vimplug_dir"
 
-echo "Installing liquidprompt..."
-git clone https://github.com/nojhan/liquidprompt.git "$liquidprompt_dir"
-
-
-echo "Installing vim-plug"
-git clone https://github.com/junegunn/vim-plug.git "$vimplug_dir"
-ln -s "$vimplug_dir/plug.vim" "$basedir/vim/autoload/plug.vim"
-
-
-link_dotfile "bashrc"
-link_dotfile "vimrc"
-link_dotfile "vim"
-link_dotfile "gitconfig"
+link_dotfile "$basedir/bashrc" "$targetdir/.bashrc"
+link_dotfile "$basedir/vim" "$targetdir/.vim"
+link_dotfile "$basedir/vimrc" "$targetdir/.vimrc"
+link_dotfile "$basedir/neovim_init.vim" "$targetdir/.config/nvim/init.vim"
+link_dotfile "$vimplug_dir/plug.vim" "$basedir/vim/autoload/plug.vim"
+link_dotfile "$basedir/gitconfig" "$targetdir/.gitconfig"
